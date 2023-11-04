2fc4d5f58303a9e20be3ee79b8a86c506610ac1a
juyeon6069 <jynam6069@gmail.com>
2023-11-03 21:14:58 -0400

    test webpage uploading(GET method)

--- db/test.js
+++ db/test.js
@@ -1,3 +1,10 @@
 const request = require('supertest');
 const fs = require("fs");
 const app = require('./server');
+
+describe('GET /', () => {
+    test('upload webpage', async () => {
+        const response = await request(app).get('/');
+        expect(response.status).toEqual(200);
+    });
+});
