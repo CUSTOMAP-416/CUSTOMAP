7a69ca11ab06255f0b66482c8ada84780a8b10f4
juyeon6069 <jynam6069@gmail.com>
2023-11-03 21:38:48 -0400

    setting route for map with HTTP methods

--- db/server.js
+++ db/server.js
@@ -56,4 +56,82 @@ app.put('/api/user/:id', (req, res) => {
     res.status(201).json(updatedUser);
 });
 
+// getMap
+app.get('/api/map/:email/:id', (req, res) => {
+    res.status(200).json({ message: 'Success' });
+})
+
+// getAllMaps
+app.get('/api/maps', (req, res) => {
+    res.status(200).json({ message: 'Success' });
+})
+
+// createMap
+app.post('/api/map', (req, res) => {
+    const { email, name, chat, file, font, color, legend } = req.body;
+    if (!file) {
+        return res.status(400).json({ message: 'File is required.' });
+    }
+    const newMap = {
+        name: name || 'myMap', 
+        chat: [{}], 
+        file: file || {},
+        font: font || '',
+        color: color || {},
+        legend: legend || ''
+    };
+
+    res.status(201).json(newMap);
+});
+
+// updateMap
+app.put('/api/map/:email/:id', (req, res) => {
+    if(!req.params.email){
+        res.status(404).json({})
+    }
+    if(!req.params.id){
+        res.status(404).json({})
+    }
+
+    //const { email, name, chat, file, font, color, legend } = req.body;
+
+    //temp
+    const name= 'Updated Test Map';
+    const chat= []
+    const file= {}
+    const font= 'Arial'
+    const color= {}
+    const legend= 'Updated legend'
+
+    const updatedMap = {
+        id: req.params.id,
+        name: name,
+        chat: chat, 
+        file: file,
+        font: font,
+        color: color,
+        legend: legend
+    };
+
+    res.status(200).json(updatedMap);
+});
+
+// deleteMap
+app.delete('/api/map/:email/:id ', (req, res) => {
+    if(!req.params.email){
+        res.status(404).json({})
+    }
+    if(!req.params.id){
+        res.status(404).json({})
+    }
+
+    const {file} = req.body;
+    const mapId = req.params.id;
+    if (!file) {
+        return res.status(400).json({ message: 'File is required.' });
+    }
+    res.status(200).json({ message: 'File is required.' });
+})
+
+
 module.exports = app; 
