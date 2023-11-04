9abcf268c0d2554779c6f93076314b8aa693970a
juyeon6069 <jynam6069@gmail.com>
2023-11-03 21:35:50 -0400

    setting route for webpage with GET method

--- db/server.js
+++ db/server.js
@@ -1,4 +1,11 @@
 const express= require('express');
 const app = express();
 
+// webpage
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: "success",
+    });
+});
+
 module.exports = app; 
