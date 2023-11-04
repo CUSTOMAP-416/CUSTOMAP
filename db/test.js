ed4dcd93986442b57b3d4d41553c6c064489f748
juyeon6069 <jynam6069@gmail.com>
2023-11-03 21:17:25 -0400

    test getUser and getAllUsers(GET method)

--- db/test.js
+++ db/test.js
@@ -8,3 +8,17 @@ describe('GET /', () => {
         expect(response.status).toEqual(200);
     });
 });
+
+describe('GET /api/user/:id', () => {
+    test('get specific user', async () => {
+        const response = await request(app).get('/api/user/:id');
+        expect(response.status).toEqual(200);
+    });
+});
+
+describe('GET /api/users', () => {
+    test('get all users', async () => {
+        const response = await request(app).get('/api/users');
+        expect(response.status).toEqual(200);
+    });
+});
