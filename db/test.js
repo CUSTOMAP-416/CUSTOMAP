74e5aeec126ebf88a9fc26906b3eb9de1838fa25
juyeon6069 <jynam6069@gmail.com>
2023-11-03 21:18:29 -0400

    test createUser(POST method)

--- db/test.js
+++ db/test.js
@@ -22,3 +22,27 @@ describe('GET /api/users', () => {
         expect(response.status).toEqual(200);
     });
 });
+
+// This is test code for createUser. After connect to front-end server, can use this
+// describe('POST /api/users', () => {
+//     test('creates a new user', async () => {
+//         const userData = {
+//             name: 'John Doe', 
+//             phone: '631-XXX-XXXX', 
+//             id: 'johndoe000', 
+//             email: 'john.doe@stonybrook.edu', 
+//             password: '0000'};
+
+//         const response = await request(app)
+//             .post('/api/users')
+//             .send(userData)
+//             .expect(201);
+//             expect(response.body).toEqual(expect.objectContaining({
+//                 name: userData.name,
+//                 phone: userData.phone,
+//                 email: userData.email,
+//                 id: userData.id,
+//             }))
+            
+//     });
+// });
