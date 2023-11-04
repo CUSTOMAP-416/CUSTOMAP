e1987a5b5f5fb09ab8215ff2284a637462640dba
juyeon6069 <jynam6069@gmail.com>
2023-11-03 21:37:52 -0400

    setting route for user with HTTP methods

--- db/server.js
+++ db/server.js
@@ -8,4 +8,52 @@ app.get('/', (req, res) => {
     });
 });
 
+// getUser
+app.get('/api/user/:id', (req, res) => {
+    res.status(200).json({ message: 'Success' });
+})
+
+// getAllUser
+app.get('/api/users', (req, res) => {
+    res.status(200).json({ message: 'Success' });
+})
+
+// createUser
+app.post('/api/users', (req, res) => {
+    const { name, phone, email, password } = req.body;
+
+    if (!email || !name || !password) {
+        return res.status(400).json({ message: 'Name, email, and password are required.' });
+    }
+
+    const newUser = {name: 'John Doe', 
+                    phone: '631-XXX-XXXX', 
+                    id: 'johndoe000', 
+                    email: 'john.doe@stonybrook.edu', 
+                    password: '0000'}
+
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
+// updateUser
+app.put('/api/user/:id', (req, res) => {
+    if(!req.params.id){
+        res.status(404).json({})
+    }
+    //const { name, phone, email, password } = req.body; 
+    
+    //temp
+    const name= 'Jane Doe'
+    const phone= '631-YYY-YYYY'
+    const password= '1234'
+
+    const updatedUser = {
+        name: name, 
+        phone: phone, 
+        password: password}
+
+    res.status(201).json(updatedUser);
+});
+
 module.exports = app; 
