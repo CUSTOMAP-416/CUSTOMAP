e88307023f0eb5d4f97f80d14b809042882a03a6
WENSHIHAOO <99094977+WENSHIHAOO@users.noreply.github.com>
2023-11-08 13:02:49 -0500

    Update index.js

--- project/server/index.js
+++ project/server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 // CREATE OUR SERVER
-const PORT = 8080;
+const PORT = process.env.PORT || 80;
 const app = express()
 
 // SETUP THE MIDDLEWARE
