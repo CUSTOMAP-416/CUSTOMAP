e797e85a7f3d0418d784e48a373f047740be211c
SeolheeYun <118879024+SeolheeYun@users.noreply.github.com>
2023-11-10 03:19:53 -0500

    create Signup.css

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,6 +1,8 @@
 import { useContext, useState } from 'react';
 import AuthStoreContextProvider from '../auth_store';
 import { Link } from "react-router-dom";
+import "../styles/SignUp.css";
+import logo from "../assets_img/signup_logo.svg";
 
 export default function SignUp(){
     const { auth_store } = useContext(AuthStoreContextProvider);
@@ -59,20 +61,70 @@ export default function SignUp(){
     }
 
     return (
-        <div className="inup">
-            <h2>ID</h2>
-            <input type="text" value={ID} onChange={handleIDChange}></input>
-            <h2>Password</h2>
-            <input type="text" value={password} onChange={handlePasswordChange}></input>
-            <h2>Password Verify</h2>
-            <input type="text" value={passwordVerify} onChange={handlePasswordVerifyChange}></input>
-            <h2>Name</h2>
-            <input type="text" value={name} onChange={handleNameChange}></input>
-            <h2>Email</h2>
-            <input type="text" value={email} onChange={handleEmailChange}></input>
-            <h2>Phone</h2>
-            <input type="text" value={phone} onChange={handlePhoneChange}></input>
-            <Link to='/Dashboard/' onClick={() => handleCreateAccount()}>Create Account</Link>
+      <div className="inup">
+        <div className="head">
+          <img className="logo" src={logo} alt="My SVG" />
+          <h1>Make New Account</h1>
         </div>
-        )
+        <div className="bodys">
+          <div className="leftText">
+            <h2>ID</h2>
+            <h2>Password</h2>
+            <h2>Password Verify</h2>
+            <h2>Name</h2>
+            <h2>Email</h2>
+            <h2>Phone</h2>
+          </div>
+          <div className="rightBox">
+            <div>
+              <input type="text" value={ID} onChange={handleIDChange}></input>
+            </div>
+            <div>
+              <input
+                type="text"
+                value={password}
+                onChange={handlePasswordChange}
+              ></input>
+            </div>
+            <div>
+              <input
+                type="text"
+                value={passwordVerify}
+                onChange={handlePasswordVerifyChange}
+              ></input>
+            </div>
+            <div>
+              <input
+                type="text"
+                value={name}
+                onChange={handleNameChange}
+              ></input>
+            </div>
+            <div>
+              <input
+                type="text"
+                value={email}
+                onChange={handleEmailChange}
+              ></input>
+            </div>
+            <div>
+              <input
+                type="text"
+                value={phone}
+                onChange={handlePhoneChange}
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Link
+            className="createB"
+            to="/Dashboard/"
+            onClick={() => handleCreateAccount()}
+          >
+            Create Account
+          </Link>
+        </div>
+      </div>
+    );
 }
