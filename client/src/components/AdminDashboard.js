ca41300b7792c7509d023b03bbb888f36bc506eb
juyeon6069 <jynam6069@gmail.com>
2023-11-11 17:22:16 -0500

    add css of user dashboard, admin dashboard

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,8 +1,14 @@
+import '../styles/AdminDashboard.css';
+
 import { useContext, useState } from 'react';
 import AuthStoreContextProvider from '../auth_store';
 
 import AdminDashboardUserList from './AdminDashboardComponents/AdminDashboardUserList';
 import AdminDashboardMapList from './AdminDashboardComponents/AdminDashboardMapList';
+
+import user from "../assets_img/adminDashboard_user.svg";
+import adminBear from "../assets_img/adminDashboard_bear.svg";
+import arrow from "../assets_img/dashboard_arrow.svg";
 
 export default function AdminDashboard(){
     const { auth_store } = useContext(AuthStoreContextProvider);
@@ -22,11 +28,61 @@ export default function AdminDashboard(){
 
     return (
         <div>
-            <div>
+            <div className = "whole-body">
+            <div className='sidebar'>
+            <img className="admin-bear" src={adminBear} style={{padding:"30px 0px"}}alt="My SVG" />
+                <div className='profile'>Hello, Admin</div>
+                <div className="sidebar-buttons">
                 <button type="button" onClick={() => handleSelectedViewChange(<AdminDashboardUserList/>)}>User List</button>
+                </div>
+                <div className="sidebar-buttons">
                 <button type="button" onClick={() => handleSelectedViewChange(<AdminDashboardMapList/>)}>Map List</button>
+                </div>
             </div>
-                {selectedView}
+            <div className='right-body'>
+            <div className="admin-header">
+                <h1 className='header-font'>Admin Dashboard</h1>
+            </div>
+            <div class="user-list">
+                <div className='sort-buttons'>
+                    <button className='arrow-button'><img className="arrow" src={arrow} alt="My SVG" /></button><button class="sort-button">Ascending</button>
+                    <button className='arrow-button'><img className="arrow" src={arrow} alt="My SVG" /></button><button class="sort-button">Descending</button>
+                    <button className='arrow-button'><img className="arrow" src={arrow} alt="My SVG" /></button><button class="sort-button">Date</button>
+                </div>
+                <div class="user-item">
+                <div style={{display: "flex", alignItems: "center"}}> 
+                <img className="user" src={user} style={{paddingRight:"20px"}} alt="My SVG" />
+                    <span>User Name 1</span></div>
+                    <div>
+                        <button className="map">Map</button>
+                        <button className="edit">Edit</button>
+                        <button className="delete">Delete</button>
+                    </div>
+                </div>
+                <div className="user-item">
+                <div style={{display: "flex", alignItems: "center"}}> 
+                <img className="user" src={user} style={{paddingRight:"20px"}} alt="My SVG" />
+                    <span>User Name 2</span></div>
+                    <div>
+                        <button className="edit">Edit</button>
+                        <button className="delete">Delete</button>
+                    </div>
+                </div>
+                <div className="user-item">
+                    <div style={{display: "flex", alignItems: "center"}}> 
+                <img className="user" src={user} style={{paddingRight:"20px"}} alt="My SVG" />
+                    <span>User Name 3</span>
+                    </div>
+                    <div>
+                        
+                        <button className="edit">Edit</button>
+                        <button className="delete">Delete</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+        {/* {selectedView} */}
         </div>
     )
 }
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,3 +1,5 @@
+import '../styles/Dashboard.css';
+
 import { useContext, useState } from 'react';
 import AuthStoreContextProvider from '../auth_store';
 
@@ -5,6 +7,11 @@ import DashboardMapListView from './DashboardComponents/DashboardMapListView';
 import DashboardMyProfileView from './DashboardComponents/DashboardMyProfileView';
 import DashboardCreateOrEditMapView from './DashboardComponents/DashboardCreateOrEditMapView';
 import DashboardSearchMapView from './DashboardComponents/DashboardSearchMapView';
+
+import bear from "../assets_img/dashboard_bear.svg";
+import arrow from "../assets_img/dashboard_arrow.svg";
+import map from "../assets_img/dashboard_map.svg";
+
 
 export default function Dashboard(){
     const { auth_store } = useContext(AuthStoreContextProvider);
@@ -23,15 +30,46 @@ export default function Dashboard(){
     }
 
     return (
-        <div>
-            <div>
+        <div className='container'>
+            <div className='sidebar'>
+            <img className="bear" src={bear} style={{padding:"30px 0px"}}alt="My SVG" />
+                <div className='profile'>Hello User</div>
+                <div className="sidebar-buttons">
                 <button type="button" onClick={() => handleSelectedViewChange(<DashboardMapListView />)}>Dashboard</button>
+                </div>
+                <div className="sidebar-buttons">
                 <button type="button" onClick={() => handleSelectedViewChange(<DashboardMyProfileView />)}>My Profile</button>
+                </div>
+                <div className="sidebar-buttons">
                 <button type="button" onClick={() => handleSelectedViewChange(<DashboardCreateOrEditMapView />)}>Create Map</button>
+                </div>
+                <div className="sidebar-buttons">
                 <button type="button" onClick={() => handleSelectedViewChange(<DashboardSearchMapView />)}>Search Map</button>
+                </div>
             </div>
-                {selectedView}
+            <div style={{width: "100%"}}>
+                <div className='dashboard-header'>
+                    Dashboard
+                </div>
+                <div className='description-and-sorting'>
+                    <div className='description'>Maps you have participated in</div>
+                <div className='sort-buttons'>
+                    <button className='arrow-button'><img className="arrow" src={arrow} alt="My SVG" /></button><button class="sort-button">Ascending</button>
+                    <button className='arrow-button'><img className="arrow" src={arrow} alt="My SVG" /></button><button class="sort-button">Descending</button>
+                    <button className='arrow-button'><img className="arrow" src={arrow} alt="My SVG" /></button><button class="sort-button">Date</button>
+                </div>
+                </div>
+                <div className="box-container">
+                    <div className='boxes'><div className="box"><img className="map" src={map} alt="My SVG" /><div style={{display: "flex", justifyContent: "center"}}><div className='map-name'>MyMap1</div><button className="delete">Delete</button></div></div></div>
+                    <div className='boxes'><div className="box"><img className="map" src={map} alt="My SVG" /><div style={{display: "flex", justifyContent: "center"}}><div className='map-name'>MyMap2</div><button className="delete">Delete</button></div></div></div>
+                    <div className='boxes'><div className="box"><img className="map" src={map} alt="My SVG" /><div style={{display: "flex", justifyContent: "center"}}><div className='map-name'>MyMap3</div><button className="delete">Delete</button></div></div></div>
+                    <div className='boxes'><div className="box"><img className="map" src={map} alt="My SVG" /><div style={{display: "flex", justifyContent: "center"}}><div className='map-name'>MyMap4</div><button className="delete">Delete</button></div></div></div>
+                    <div className='boxes'><div className="box"><img className="map" src={map} alt="My SVG" /><div style={{display: "flex", justifyContent: "center"}}><div className='map-name'>MyMap5</div><button className="delete">Delete</button></div></div></div>
+                </div>
+                    {/* {selectedView} */}
+            </div>
         </div>
+
     )
 
 }
