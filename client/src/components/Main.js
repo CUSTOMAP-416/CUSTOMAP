147f61ef78e638dfc3760df932b92b5059fa06d6
Weikang Yang <96400625+HarryYang1008@users.noreply.github.com>
2023-11-12 05:16:40 -0500

    welcome page

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,14 +1,27 @@
 import { useContext } from 'react';
 import AuthStoreContextProvider from '../auth_store';
 import Dashboard from './Dashboard';
+import image from "./MainWelcome.png"
 
 export default function Main() {
     const { auth_store } = useContext(AuthStoreContextProvider);
     console.log("Main auth_store.loggedIn: " + auth_store.loggedIn);
+    const imageStyle = {
+        width: '100%',
+        height: '82%',
+        objectFit: 'cover', // Cover the container without stretching
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)' // Center the image
+      };
     
-    if (auth_store.loggedIn)
+    if(auth_store.loggedIn)
         return <Dashboard />
     else
-        return  <p>CUSTOMAP</p>
+        
+        return (
+            <img id="welcome"src={image} alt="welcom to the customap" style={imageStyle} ></img>
+        )
 
 }
