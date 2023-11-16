6a0a7c098bbf4fdfc6a113b4b45f2b4b8527cff4
WENSHIHAOO <99094977+WENSHIHAOO@users.noreply.github.com>
2023-11-16 17:01:14 -0500

    catch error

--- client/src/auth_store/index.js
+++ client/src/auth_store/index.js
@@ -75,222 +75,266 @@ function AuthStoreContextProvider(props) {
     }
 
     auth_store.getUser = async function () {
-        const response = await apis.getUser();
-        if (response.status === 200) {
+        await apis.getUser().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.GET_LOGGED_IN,
                 payload: response.data.user,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //All user list in Admin dashboard const getUsers = async () => { ?
     auth_store.getAllUsers = async function () {
-        const response = await apis.getAllUsers();
-        if (response.status === 200) {
+        await apis.getAllUsers().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     // Registers the user const onSignUp = async (userData) => ?
     auth_store.createUser = async function (state) {
-        const response = await apis.createUser(state.name, state.phone, state.id, state.email, state.password, state.passwordVerify);
-        if (response.status === 200) {
+        await apis.createUser(state.name, state.phone, state.id, state.email, state.password, state.passwordVerify).then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //Change the profile user const onChangeInformation = async (userData) => {...
     //function to handle change password process const onChangePassword = async () => ?
     auth_store.updateUser = async function () {
-        const response = await apis.updateUser();
-        if (response.status === 200) {
+        await apis.updateUser().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     ///All map list in Admin dashboard const getMaps = async () => { ?
     auth_store.getAllMaps = async function () {
-        const response = await apis.getAllMaps();
-        if (response.status === 200) {
+        await apis.getAllMaps().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     auth_store.getMap = async function () {
-        const response = await apis.getMap();
-        if (response.status === 200) {
+        await apis.getMap().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the create a new map process const onCreateMap = async (map) => { ?
     auth_store.createMap= async function () {
-        const response = await apis.createMap();
-        if (response.status === 200) {
+        await apis.createMap().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.REGISTER_USER,
                 payload: response.data.user,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     auth_store.updateMap= async function () {
-        const response = await apis.updateMap();
-        if (response.status === 200) {
+        await apis.updateMap().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle delete map process. const onDeleteMap = async (map) => { ?
     auth_store.deleteMap= async function () {
-        const response = await apis.deleteMap();
-        if (response.status === 200) {
+        await apis.deleteMap().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     // Logs in the user const onLogin = async (userData) => { ?
     auth_store.onLogin= async function (state) {
-        const response = await apis.loggedIn(state.email, state.password);
-        if (response.status === 200) {
+        await apis.loggedIn(state.email, state.password).then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.LOGIN_USER,
                 payload: response.data.user,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     // Logs out the user
     auth_store.onLogout= async function () {
-        const response = await apis.onLogout();
-        if (response.status === 200) {
+        await apis.onLogout().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.LOGOUT_USER,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle verification process 
     auth_store.onVerification= async function (state) {
-        const response = await apis.onVerification(state.username, state.email, state.phone);
-        if (response.status === 200) {
+        await apis.onVerification(state.username, state.email, state.phone).then(response => {
             auth_storeReducer({
               type: AuthStoreActionType.null,
               payload: response.data.user,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the edit map process 
     auth_store.onEditMap= async function () {
-        const response = await apis.onEditMap();
-        if (response.status === 200) {
+        await apis.onEditMap().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the fork map process 
     auth_store.onForkMap= async function () {
-        const response = await apis.onForkMap();
-        if (response.status === 200) {
+        await apis.onForkMap().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the attach property process 
     auth_store.onAttachProperty= async function () {
-        const response = await apis.onAttachProperty();
-        if (response.status === 200) {
+        await apis.onAttachProperty().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the Discussion process 
     auth_store.onDiscussion= async function () {
-        const response = await apis.onDiscussion();
-        if (response.status === 200) {
+        await apis.onDiscussion().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the text process 
     auth_store.onText= async function () {
-        const response = await apis.onText();
-        if (response.status === 200) {
+        await apis.onText().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the Color process 
     auth_store.onColor= async function () {
-        const response = await apis.onColor();
-        if (response.status === 200) {
+        await apis.onColor().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the Legend process 
     auth_store.onLegend= async function () {
-        const response = await apis.onLegend();
-        if (response.status === 200) {
+        await apis.onLegend().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the search process 
     auth_store.onSearch= async function () {
-        const response = await apis.onSearch();
-        if (response.status === 200) {
+        await apis.onSearch().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle get the Array Discussions. 
     auth_store.getArrayDiscussions= async function () {
-        const response = await apis.getArrayDiscussions();
-        if (response.status === 200) {
+        await apis.getArrayDiscussions().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle getting the list of user's created maps. 
     auth_store.getUserMaps= async function () {
-        const response = await apis.getUserMaps();
-        if (response.status === 200) {
+        await apis.getUserMaps().then(response => {
             auth_storeReducer({
                 type: AuthStoreActionType.null,
                 payload: null,
             });
-        }
+        })
+        .catch(error => {
+            console.log(error.response.data.errorMessage)
+        });
     }
     //function to handle the redo process. 
     auth_store.onRedo = function () {}
