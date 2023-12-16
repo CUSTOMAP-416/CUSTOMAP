bbc1918247ef6a883feb4cc74ee436c2bb40a72c
juyeon6069 <jynam6069@gmail.com>
2023-12-15 23:38:13 -0500

    Update main page

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -9,7 +9,9 @@ import edit from "../assets_img/main-2.png";
 import share from "../assets_img/main-3.png";
 import search from "../assets_img/main-4.png";
 import community from "../assets_img/main-5.png";
+import selection from "../assets_img/main-6.png";
 import icon from "../assets_img/icon.svg";
+import second from "../assets_img/secondpage.png";
 import "../styles/Main.css"
 
 export default function Main() {
@@ -26,60 +28,72 @@ export default function Main() {
             </div>
             <div className='main-subtitle-logo'>Explore and Customize Your Own Map!</div>
         </div>
-    <div style={{backgroundColor:"#e6ffe6", padding: "50px 0px"}}>   
+    <div style={{backgroundColor:"rgb(236 249 236)", padding: "50px 0px"}}>   
         <div className="main-header" style={{border: "#012c01", borderWidth: "4px"}}>
-            Welcome to CUSTOMAP, Explore and Customize Your own Map!
+            <span>Welcome to</span> <span style={{color: "#006400"}}>CUSTOMAP</span><span>, Explore and Customize Your Own Map!</span>
         </div>
-        <div className="main-content">
-            Start on a journey of discovery and creativity with our innovative web application. At CUSTOMAP, we transcend the boundaries of conventional map-making, offering a dynamic platform for creating, editing, and sharing custom map graphics.
-        </div>
+        <div className="main-content" style={{textAlign: "left", paddingLeft: "50px", paddingBottom: "40px"}}>
+            <div>Start on a journey of discovery and creativity with our platform. </div>
+            <div>With CUSTOMAP, you can create, edit, and share map as you want beyond the boundaries of the traditional map creation technique.</div>        </div>
         <img className="main-laptop" src={laptop} alt="My SVG" />
     </div>
-    <div className="main-header" style={{textAlign: "left", paddingTop: "30px"}}>Why Choose CUSTOMAP?</div>
+    <div className="main-header" style={{textAlign: "left", paddingTop: "40px", color: "#104502", fontSize: "55px"}}>Why Choose CUSTOMAP?</div>
+    
+    <div className="main-content-items">
     <div className='main-flex'>
+    <img className="main-image" src={create} alt="My SVG" />
     <div className="main-subtitle">
-        Map Graphic Creation: 
+        Map Graphic Creation 
         <div className="main-feature">
         Upload SHP/DBF (ZIP), GeoJSON, KML files, or fork existing maps on the platform to create map graphics.
     </div>
     </div>
-    <img className="main-image" src={create} alt="My SVG" />
+    </div>
+    <div className='main-flex'>
+    <img className="main-image" src={selection} alt="My SVG" />
+    <div className="main-subtitle">
+        Map Graphic Selection
+        <div className="main-feature">
+        Select various map type you want to create! You can choose among Default, Heat, Point, Bubble, Thematic, and Choropleth map.
+    </div>
+    </div>
     </div>
     <div className='main-flex'>
     <img className="main-image" src={edit} alt="My SVG" />
     <div className="main-subtitle">
-        Map Graphic Editing: 
+        Map Graphic Editing 
     <div className="main-feature">
         Graphic editing (text, color, legend), map viewing and navigation, naming regions, attaching custom data properties, and adding decorations.
     </div>
     </div>
     </div>
     <div className='main-flex'>
+    <img className="main-image" src={share} alt="My SVG" />
     <div className="main-subtitle">
-    Map Graphics Exporting:
+    Map Graphics Exporting
     <div className="main-feature">
         Provides map export in PNG, JPG and JSON formats.
     </div>
     </div>
-    <img className="main-image" src={share} alt="My SVG" />
     </div>
     <div className='main-flex'>
     <img className="main-image" src={search} alt="My SVG" />
     <div className='main-subtitle'>
-    Map Classification and Search:
+    Map Classification and Search
     <div className="main-feature">
         Map classification and search functions based on properties.
     </div>
     </div>
     </div>
     <div className='main-flex'>
+    <img className="main-image" src={community} alt="My SVG" />
     <div className='main-subtitle'>
-    Community Interactions:
+        Community Interactions
     <div className="main-feature">
         Users discuss maps, give feedback, and collaborate through map sharing.
     </div>
     </div>
-    <img className="main-image" src={community} alt="My SVG" />
+    </div>
     </div>
     <div className="main-footer">
         Join CUSTOMAP and interact with maps. Create, share, and explore the limitless possibilities of custom map graphics today!
